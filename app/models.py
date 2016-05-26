0d5a4dd61c7a4ae9dd9b8ed96235e997ef5dfa53
Emalindah Kimari <emalindahkimari@gmail.com>
2016-05-26 10:43:09 +0300

    Configured Mail Settings

---
+++ app/models.py
@@ -0,0 +1,99 @@
+import datetime
+from flask import Flask
+from peewee import *
+from playhouse.flask_utils import FlaskDB, get_object_or_404, object_list
+from playhouse.sqlite_ext import *
+from micawber import bootstrap_basic, parse_html
+from micawber.cache import Cache as OEmbedCache
+from markdown import markdown
+from markdown.extensions.codehilite import CodeHiliteExtension
+from markdown.extensions.extra import ExtraExtension
+from bs4 import BeautifulSoup
+#import pdb; pdb.set_trace()
+from app import *
+
+
+app.config.from_object(__name__)
+db = FlaskDB(app)
+database = db.database
+
+
+#login_manager = LoginManager()
+class Entry(db.Model):
+    title = CharField()
+    slug = CharField(unique=True)
+    content = TextField()
+    published = BooleanField(index=True)
+    timestamp = DateTimeField(default=datetime.datetime.now, index=True)
+    #try to use moments instead!!!!!
+    @property
+    def html_content(self):
+        hilite = CodeHiliteExtension(linenums=False, css_class='highlight')
+        extras = ExtraExtension()
+        markdown_content = markdown(self.content, extensions=[hilite, extras])
+        oembed_content = parse_html(
+            markdown_content,
+            oembed_providers,
+            urlize_all=True,
+            maxwidth=app.config['SITE_WIDTH'])
+        return Markup(oembed_content)
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = re.sub('[^\w]+', '-', self.title.lower()).strip('-')
+        ret = super(Entry, self).save(*args, **kwargs)
+        self.update_search_index()
+        return ret
+
+    def update_search_index(self):
+        try:
+            fts_entry = FTSEntry.get(FTSEntry.entry_id == self.id)
+        except FTSEntry.DoesNotExist:
+            fts_entry = FTSEntry(entry_id=self.id)
+            force_insert = True
+        else:
+            force_insert = False
+        fts_entry.content = '\n'.join((self.title, self.content))
+        fts_entry.save(force_insert=force_insert)
+
+    @classmethod
+    def public(cls):
+        return Entry.select().where(Entry.published == True)
+
+    @classmethod
+    def drafts(cls):
+        return Entry.select().where(Entry.published == False)
+
+    @classmethod
+    def search(cls, query):
+        words = [word.strip() for word in query.split() if word.strip()]
+        if not words:
+            return Entry.select().where(Entry.id == 0)
+        else:
+            search = ' '.join(words)
+
+        
+        return (FTSEntry
+                .select(
+                    FTSEntry,
+                    Entry,
+                    FTSEntry.rank().alias('score'))
+                .join(Entry, on=(FTSEntry.entry_id == Entry.id).alias('entry'))
+                .where(
+                    (Entry.published == True) &
+                    (FTSEntry.match(search)))
+                .order_by(SQL('score').desc()))
+
+class FTSEntry(FTSModel):
+    entry_id = IntegerField(Entry)
+    content = TextField()
+
+    class Meta:
+        database = database
+
+def main():
+    database.create_tables([Entry, FTSEntry], safe=True)
+    app.run(debug=True)
+
+if __name__ == '__main__':
+    main()
